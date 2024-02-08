10bcc37460ec24df524c99e327c98a2a6cbf5b5a
Humaid Alam Ansari <117574223+28humaid@users.noreply.github.com>
2024-02-08 12:53:47 +0530

    Update knnAlgo2611.py

--- knnAlgo2611.py
+++ knnAlgo2611.py
@@ -1,9 +1,8 @@
-## KNN algorithm dated 26/11/2022
-
 import random #to generate random numbers
 import matplotlib.pyplot as plt #to draw the scatter plot
 import math #to calculate euclidean distance
 
+#creating a randomly generated dataset
 datasetDict={}
 for i,j in zip(range(100),range(200)):
   toTuple=(random.randrange(0,i+1),random.randrange(0,j+1))
@@ -22,6 +21,7 @@ for i in datasetDict:
 print('count of 0 : ',count0)
 print('count of 1 : ',count1)
 
+#plot the points on graph for visualization
 x_0=[]
 y_0=[]
 x_1=[]
@@ -33,13 +33,11 @@ for i in datasetDict:
   elif datasetDict[i] == 1:
     x_1.append(i[0])
     y_1.append(i[1])
-  
-
 plt.scatter(x_0,y_0,color='red')
 plt.scatter(x_1,y_1,color='violet')
-#bina plt.show() k bhi print kr de rha hai but ek message aur print hokr arha hai
-plt.show()
+plt.show()  
 
+#dividing the dataset into training and testing part
 trainingDataset={}
 testingDataset={}
 for i in datasetDict:
@@ -52,6 +50,7 @@ for i in datasetDict:
 print('no. of elements in training dataset : ',len(trainingDataset))
 print('no. of elements in testing dataset : ',len(testingDataset))
 
+#function to calculate the euclidean distance
 def euclideanDistance(A,B):
   sum=0
   for a,b in zip(A,B):
@@ -60,26 +59,26 @@ def euclideanDistance(A,B):
   return sum**0.5
 
 def knnAlgorithm(dataItem,k):
-  #is dictionary ka key dataItem hoga aur value distance hoga training dataset k elements se
+  #"key" for this dictionary will be the dataIten and the "value" will be the distance from training set elements
   distanceDictionary={}
   for i in trainingDataset:
     distanceDictionary[i]=euclideanDistance(dataItem,i)
-  #ab is dictionary ko sort kr do values (distance) k basis pr
+  #now sort this dictionary in increasing order of the distance
   sortedDistanceDictionary=dict(sorted(distanceDictionary.items(),key=lambda x:x[1]))
 
-  #training dataset se 0/1 ki values sorted dictionary k elements ko assign kr do
+  #now assign the values of classes to this sorted dictionary
   for i in sortedDistanceDictionary:
     for j in trainingDataset:
       if i == j:
         sortedDistanceDictionary[i]=trainingDataset[j]
-  
-  #ab top k 'k' nearest neighbors le lo
+
+  #now take into consideration the nearest 'k' neighbours based on this sorted dictionary
   knnDictionary={}
   for i in sortedDistanceDictionary:
     if len(knnDictionary)<k:
       knnDictionary[i]=sortedDistanceDictionary[i]
 
-  #let's vote : majority voting
+  #do the majority voting
   count_0=0
   count_1=0
   for i in knnDictionary:
@@ -88,32 +87,35 @@ def knnAlgorithm(dataItem,k):
     else:
       count_1+=1
 
-  #jo bda hai usey return krdo
   if count_0 > count_1:
     return 0
   else:
     return 1
 
-predictedCorrectly=0
-for item in testingDataset:
-    if testingDataset[item]==knnAlgorithm(item,3):
-      predictedCorrectly+=1
+#train the model to find the value of 'k'
+accuracy_list=[]
+for i in range(15):
+  predictedCorrectly=0
+  for item in testingDataset:
+      if testingDataset[item]==knnAlgorithm(item,i):
+        predictedCorrectly+=1
+  print('accuracy = ',(predictedCorrectly/len(testingDataset))*100)
+  accuracy_list.append((predictedCorrectly/len(testingDataset))*100)
 
-print('accuracy = ',(predictedCorrectly/len(testingDataset))*100)
+k=accuracy_list.index(max(accuracy_list))+1
+print('value of k is ',k)
 
+#take user input data point to determine its class
 x=int(input('Enter x coordinate : '))
 y=int(input('Enter y coordinate : '))
 
 UserDefinedTuple=(x,y)
 
-#first show it on scatter plot along with training and testing dataset
+#show it on scatter plot along with training and testing dataset
 plt.scatter(x,y,color='green')
 plt.scatter(x_0,y_0,color='red')
 plt.scatter(x_1,y_1,color='violet')
 
-#bina plt.show() k bhi print kr de rha hai but ek message aur print hokr arha hai
 plt.show()
 
-
-print('class of ',UserDefinedTuple,' : ',knnAlgorithm(UserDefinedTuple,3))
-
+print('class of ',UserDefinedTuple,' : ',knnAlgorithm(UserDefinedTuple,k))
